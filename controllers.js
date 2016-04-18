b789703332ca188a546f9fe0410c94be7a7a8a7a
Pete Burch <peteburch223@gmail>
2016-04-18 15:55:35 +0100

    #4 completed

--- controllers.js
+++ controllers.js
@@ -1,4 +1,13 @@
 toDoApp.controller('ToDoController', function() {
   // we can set an attribute like any other JavaScript object
   this.todos = [{text:"ToDo1", completed:false}, {text:"ToDo2", completed:true}, {text:"ToDo3", completed:false}];
+
+  this.addToDo = function(toDo){
+    console.log(toDo);
+    this.todos.push({text:toDo, completed:false});
+  }
+
+  this.removeToDo = function(){
+    this.todos.pop();
+  }
 });
